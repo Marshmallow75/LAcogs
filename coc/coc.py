340653ebf5c5c585e21298f4e5f6f8c4cd3e11d4
Marshmallow <61973073+Marshmallow75@users.noreply.github.com>
2020-08-10 16:39:45 +1000

    Update coc.py

--- coc/coc.py
+++ coc/coc.py
@@ -28,6 +28,32 @@ class ClashOfClansCog(commands.Cog):
         url = self.baseurl + url
         response = httpx.get(url=url, headers=self.headers, timeout=20)
         return response.json()
+    
+        @commands.command()
+    async def find_member(self, ctx, member: discord.Member):
+        for u in self.config.all_users():
+            if discord.utils.escape_markdown(u.display_name.lower()) == discord.utils.escape_markdown(member.display_name.lower()):
+                await ctx.send(self.config.user(u).tag())
+        await ctx.send("No matches were found.")
+    
+    @commands.command()
+    async def get(self, ctx, tag: str):
+        """Takes in the clan's tag and returns clan info"""
+
+        tag = "clans/%23" + tag.replace("#", "")
+        clan_json = self.apirequest(tag)
+        
+        try:
+            if clan_json['clanLevel'] < 5:
+                donation_upgrade = 0
+            elif clan_json['clanLevel'] < 10:
+                donation_upgrade = 1
+            else:
+                donation_upgrade = 2
+        except:
+            return await ctx.send(clan_json)
+        
+        await ctx.send("All went good.")
 
     @commands.command()
     async def getclantest(self, ctx, tag: str):
@@ -37,5 +63,31 @@ class ClashOfClansCog(commands.Cog):
             clan_json = self.apirequest(tag)
         except Exception as e:
             return await ctx.send(e)
+        
+        class General(commands.Cog):
+   #Description of what this file does 
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="get_clan", aliases=["getclan", "clan"])
+    async def get_clan(self, ctx, clan_tag):
+        """Gets clan information from API and displays it for user"""
+        # This line uses a utility in the coc.py library to correct clan tags (case, missing #, etc.)
+        clan_tag = coc.utils.correct_tag(clan_tag)
+
+        clan = await self.bot.coc.get_clan(clan_tag)
+        content = f"The clan name for {clan_tag} is {clan.name}.\n"
+        content += f"{clan.name} currently has {clan.member_count} members.\n\n"
+
+        war = await self.bot.coc.get_current_war(clan_tag)
+        if war:
+            content += f"Current war state is {war.state}\n"
+            if war.state != "notInWar":
+                content += f"Opponent: {war.opponent}"
+
+        await ctx.send(content)
+
+def setup(bot):
+    bot.add_cog(General(bot))
 
         await ctx.send(clan_json['name'])
